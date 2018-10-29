4de439b1b43483c6d62b5985f2ef35725adf7d46
Adam Butkiewicz <abutkiewicz@egnyte.com>
2018-10-29 15:21:38 +0100

    Added test data and enabled swagger

--- src/main/java/com/okta/scim/Application.java
+++ src/main/java/com/okta/scim/Application.java
@@ -15,9 +15,28 @@
 
 package com.okta.scim;
 
+import java.util.UUID;
+
+import javax.annotation.PostConstruct;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import com.github.javafaker.Faker;
+import com.okta.scim.database.GroupDatabase;
+import com.okta.scim.database.GroupMembershipDatabase;
+import com.okta.scim.database.UserDatabase;
 import com.okta.scim.dispatchers.LoggingDispatcherServlet;
+import com.okta.scim.models.Group;
+import com.okta.scim.models.GroupMembership;
+import com.okta.scim.models.User;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletAutoConfiguration;
@@ -26,8 +45,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.DispatcherServlet;
 
 @SpringBootApplication
+@EnableSwagger2
 public class Application {
     private static Logger logger = LoggerFactory.getLogger(Application.class);
+
+    @Autowired
+    UserDatabase userDb;
+    @Autowired
+    GroupDatabase groupDb;
+    @Autowired
+    GroupMembershipDatabase groupMembershipDatabase;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
@@ -42,5 +69,60 @@ public class Application {
     @Bean(name = DispatcherServletAutoConfiguration.DEFAULT_DISPATCHER_SERVLET_BEAN_NAME)
     public DispatcherServlet dispatcherServlet() {
         return new LoggingDispatcherServlet();
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    @PostConstruct
+    public void init() {
+        Faker faker = new Faker();
+
+        Group group = generateRandomGroup(faker);
+
+        for (int i = 0; i < 103; i++) {
+            User user = generateRandomUser(faker);
+
+            generateMembership(group, user);
+        }
+
+
+    }
+
+    private void generateMembership(Group group, User user) {
+        GroupMembership groupMembership = new GroupMembership();
+        groupMembership.id = UUID.randomUUID().toString();
+        groupMembership.userId = user.id;
+        groupMembership.groupId = group.id;
+        groupMembership.groupDisplay = group.displayName;
+        groupMembership.userDisplay = user.userName;
+
+        groupMembershipDatabase.save(groupMembership);
+    }
+
+    private Group generateRandomGroup(Faker faker) {
+        Group group = new Group();
+        group.displayName = faker.lebowski().character();
+        group.id = UUID.randomUUID().toString();
+
+        return groupDb.save(group);
+    }
+
+    private User generateRandomUser(Faker faker) {
+        User user = new User();
+        user.id = UUID.randomUUID().toString();
+        user.userName = "username_" + user.id;
+        user.active = true;
+        user.familyName = faker.name().firstName();
+        user.givenName = faker.name().lastName();
+        user.middleName = faker.name().nameWithMiddle();
+
+        return userDb.save(user);
     }
 }
